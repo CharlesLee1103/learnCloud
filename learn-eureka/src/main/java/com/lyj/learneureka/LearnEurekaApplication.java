c2ae5139feb0d00e59064c3b399932fd4a7ffda2
charlesliyonjie <charlesliyongjie@didiglobal.com>
2021-03-08 11:43:37 +0800

    eureka init

---
+++ learn-eureka/src/main/java/com/lyj/learneureka/LearnEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.lyj.learneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnEurekaApplication.class, args);
+    }
+
+}
---
+++ learn-eureka/src/test/java/com/lyj/learneureka/LearnEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyj.learneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
